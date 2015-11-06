c3a97ea8844ad89e6e00191738ee1ada20dc75e7
Callum <callum@rumblelane.com>
2015-11-07 05:39:25 +1300

    Fixes for includeme.js file

--- static/js/includeme.js
+++ static/js/includeme.js
@@ -2,31 +2,34 @@
 var ImageHandler = function() {
 	
 	return {
-		onChange : function(i) {
+		onChange : function(e, i) {
 			var img = null;
-			if ( i != null )
-				img = i;
-			else
-				img = $(this);
-			console.log(img);
+			if ( e != null )
+				img = $(e.target);
+			else if ( i != null )
+				img = $(i);
 			var src = img.attr('src');
-			var regex = /^(?:http:\/\/)?pics\.rumblelane\.com.*([tsmlfTSMLF])$/ ;
+			//var regex = /^(?:http:\/\/)?pics\.rumblelane\.com\/image\/([0-9]+_[0-9])$/ ;
+			var regex = /^((?:http:\/\/)?pics\.rumblelane\.com.*)([tsmlfTSMLF])$/ ;
 			matches = src.match(regex);
-			if ( ! matches )
+			if ( ! matches || matches.length < 2 ) {
+				console.log( matches );
+				console.log( matches.length );
 				return;
+			}
 			var winWidth = $( window ).width() ;
 			if ( winWidth >= 1080 ) {
 				//replace with the largest version of the image
-				img.attr('src', src.replace(regex, 'l') );
+				img.attr('src', src.replace(regex, '$1l') );
 			}
 			else if ( winWidth > 720 ) {
-				img.attr('src', src.replace(regex, 'm') );
+				img.attr('src', src.replace(regex, '$1m') );
 			}
 			else if ( winWidth > 320 ) {
-				img.attr('src', src.replace(regex, 's') );
+				img.attr('src', src.replace(regex, '$1s') );
 			}
 			else /*if ( winWidth > 100 )*/ {
-				img.attr('src', src.replace(regex, 't') );
+				img.attr('src', src.replace(regex, '$1t') );
 			}
 		}
 	};
@@ -40,7 +43,7 @@ $('img')
 
 $( window ).resize(function() {
 	$('img').each( function() {
-		ImageHandler.onChange( $(this) );
+		ImageHandler.onChange( null, $(this) );
 	});
 });
 
